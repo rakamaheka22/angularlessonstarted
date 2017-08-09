9f710439e0e881d11af612fd88467da65805f8a2
rakamaheka <rakamaheka@gmail.com>
2017-08-09 22:24:06 +0700

    Lesson 12

--- src/app/simple-form/simple-form.component.ts
+++ src/app/simple-form/simple-form.component.ts
@@ -3,7 +3,14 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 @Component({
   selector: 'app-simple-form',
   template: `
-      <input #myInput type="text" [(ngModel)]="message">
+      <input 
+      #myInput 
+      type="text" 
+      [(ngModel)]="message"
+      [ngClass]="{mousedown:isMousedown}"
+      (mousedown)="isMousedown = true"
+      (mouseup)="isMousedown = false"
+      (mouseleave)="isMousedown = false">
       <button (click)="update.emit({text:message})">Click me!</button>
   `,
   styles: [`
@@ -14,6 +21,10 @@ import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
 
   *{
     font-family:Segoe UI;
+  }
+
+  .mousedown {
+    border:2px solid green;
   }
 
   input:focus {
